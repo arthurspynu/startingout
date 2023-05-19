94e9b4aa7d71074b843af0dfc0749676640dd40b
arthurspynu <133940373+arthurspynu@users.noreply.github.com>
2023-05-20 09:17:31 +1200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1;
+            float num2;
+            float result;
+
+            string anwser;
+
+            Console.WriteLine("Hello there! Calculator app here.");
+            Console.WriteLine("Write first number you must");
+
+
+            num1 = Convert.ToSingle(Console.ReadLine());
+
+
+            Console.WriteLine("Two numbers must there be, no more, no less");
+
+          num2 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("What calculation you after?");
+            Console.WriteLine("+ for addition, - for substraction, * for multiples, / for division");
+
+            anwser = Console.ReadLine();   
+            
+            if(anwser == "+") 
+            {
+                result = num1 + num2;
+            }
+
+          else if(anwser == "-")
+            {
+            result = num1 - num2;
+            }
+
+            else if(anwser == "*")
+            {
+                result = num1 * num2;
+            }
+
+            else
+            { result = num1 / num2;}
+
+            Console.WriteLine("Reslut here is " + result);
+            Console.WriteLine("May the math be with you!");
+            Console.ReadKey();
+        }
+    }
+}
